c6b28e66aa9c274f0bb6e9cc1b2d7bcf5f1ddcf0
RooftopMash <borulelo@gmail.com>
2025-05-23 14:31:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// src/App.js
 import React, { useState } from "react";
 import CountrySelector from "./components/CountrySelector";
 import BrandList from "./components/BrandList";
+import SuggestBrandForm from "./components/SuggestBrandForm";
 
 export default function App() {
   const [selectedCountry, setSelectedCountry] = useState("ZA");
@@ -10,6 +12,7 @@ export default function App() {
       <h1>Stash or Trash???</h1>
       <CountrySelector selectedCountry={selectedCountry} onChange={setSelectedCountry} />
       <BrandList selectedCountry={selectedCountry} />
+      <SuggestBrandForm selectedCountry={selectedCountry} />
     </div>
   );
 }
