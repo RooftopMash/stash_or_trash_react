4c398b17235af15b569517cf8373f4e9fd6860f2
RooftopMash <borulelo@gmail.com>
2025-05-23 17:03:26 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBu1iRSWC3l7VGJvHyD49xXqqGdEIa9Kis",
   authDomain: "stashortrash-acbbf.firebaseapp.com",
   projectId: "stashortrash-acbbf",
-  storageBucket: "stashortrash-acbbf.firebasestorage.app",
+  storageBucket: "stashortrash-acbbf.appspot.com",
   messagingSenderId: "782905521538",
-  appId: "1:782905521538:web:856d1e7789edd76882cb9b",
-  measurementId: "G-8Y4ZXJTPM6"
+  appId: "1:782905521538:web:9f4d415266c1f6ac82cb9b",
+  measurementId: "G-G9Z5CWWL6Z"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
