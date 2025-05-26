9023869dca8c538bb9e36bbf977704a664dca9fc
RooftopMash <borulelo@gmail.com>
2025-05-27 00:22:03 +0200

    Update countryLanguageMap.js

--- src/i18n/countryLanguageMap.js
+++ src/i18n/countryLanguageMap.js
@@ -1,1 +1,253 @@
+// Country-to-primary-native-language mapping for all ISO countries
+// ISO 3166-1 alpha-2 country codes mapped to main native language ISO codes
+// Excludes English unless it's the only native language
 
+export const countryLanguageMap = {
+  AF: "ps", // Pashto
+  AX: "sv", // Swedish
+  AL: "sq", // Albanian
+  DZ: "ar", // Arabic
+  AS: "sm", // Samoan
+  AD: "ca", // Catalan
+  AO: "pt", // Portuguese
+  AI: "en", // English (sole native)
+  AQ: "en", // English (research stations, fallback)
+  AG: "en", // English (sole native)
+  AR: "es", // Spanish
+  AM: "hy", // Armenian
+  AW: "nl", // Dutch
+  AU: "en", // English (sole native)
+  AT: "de", // German
+  AZ: "az", // Azerbaijani
+  BS: "en", // English (sole native)
+  BH: "ar", // Arabic
+  BD: "bn", // Bengali
+  BB: "en", // English (sole native)
+  BY: "be", // Belarusian
+  BE: "nl", // Dutch (most native)
+  BZ: "es", // Spanish
+  BJ: "fr", // French
+  BM: "en", // English (sole native)
+  BT: "dz", // Dzongkha
+  BO: "es", // Spanish
+  BQ: "nl", // Dutch
+  BA: "bs", // Bosnian
+  BW: "tn", // Tswana
+  BV: "no", // Norwegian
+  BR: "pt", // Portuguese
+  IO: "en", // English (sole native)
+  BN: "ms", // Malay
+  BG: "bg", // Bulgarian
+  BF: "fr", // French
+  BI: "rn", // Kirundi
+  KH: "km", // Khmer
+  CM: "fr", // French
+  CA: "fr", // French (most native after English)
+  CV: "pt", // Portuguese
+  KY: "en", // English (sole native)
+  CF: "fr", // French
+  TD: "fr", // French
+  CL: "es", // Spanish
+  CN: "zh", // Mandarin Chinese
+  CX: "en", // English (sole native)
+  CC: "ms", // Malay
+  CO: "es", // Spanish
+  KM: "ar", // Arabic
+  CG: "fr", // French
+  CD: "fr", // French
+  CK: "rar", // Rarotongan
+  CR: "es", // Spanish
+  CI: "fr", // French
+  HR: "hr", // Croatian
+  CU: "es", // Spanish
+  CW: "nl", // Dutch
+  CY: "el", // Greek
+  CZ: "cs", // Czech
+  DK: "da", // Danish
+  DJ: "aa", // Afar
+  DM: "en", // English (sole native)
+  DO: "es", // Spanish
+  EC: "es", // Spanish
+  EG: "ar", // Arabic
+  SV: "es", // Spanish
+  GQ: "es", // Spanish
+  ER: "ti", // Tigrinya
+  EE: "et", // Estonian
+  ET: "am", // Amharic
+  FK: "en", // English (sole native)
+  FO: "fo", // Faroese
+  FJ: "fj", // Fijian
+  FI: "fi", // Finnish
+  FR: "fr", // French
+  GF: "fr", // French
+  PF: "fr", // French
+  TF: "fr", // French
+  GA: "fr", // French
+  GM: "wo", // Wolof
+  GE: "ka", // Georgian
+  DE: "de", // German
+  GH: "ak", // Akan
+  GI: "en", // English (sole native)
+  GR: "el", // Greek
+  GL: "kl", // Greenlandic
+  GD: "en", // English (sole native)
+  GP: "fr", // French
+  GU: "ch", // Chamorro
+  GT: "es", // Spanish
+  GG: "en", // English (sole native)
+  GN: "fr", // French
+  GW: "pt", // Portuguese
+  GY: "gu", // Guyanese Creole (fallback: English)
+  HT: "ht", // Haitian Creole
+  HM: "en", // English (sole native)
+  VA: "it", // Italian
+  HN: "es", // Spanish
+  HK: "zh", // Cantonese Chinese
+  HU: "hu", // Hungarian
+  IS: "is", // Icelandic
+  IN: "hi", // Hindi
+  ID: "id", // Indonesian
+  IR: "fa", // Persian
+  IQ: "ar", // Arabic
+  IE: "ga", // Irish
+  IM: "gv", // Manx Gaelic
+  IL: "he", // Hebrew
+  IT: "it", // Italian
+  JM: "jam", // Jamaican Patois (fallback: English)
+  JP: "ja", // Japanese
+  JE: "en", // English (sole native)
+  JO: "ar", // Arabic
+  KZ: "kk", // Kazakh
+  KE: "sw", // Swahili
+  KI: "gil", // Gilbertese
+  KP: "ko", // Korean
+  KR: "ko", // Korean
+  KW: "ar", // Arabic
+  KG: "ky", // Kyrgyz
+  LA: "lo", // Lao
+  LV: "lv", // Latvian
+  LB: "ar", // Arabic
+  LS: "st", // Southern Sotho
+  LR: "kpe", // Kpelle (fallback: English)
+  LY: "ar", // Arabic
+  LI: "de", // German
+  LT: "lt", // Lithuanian
+  LU: "lb", // Luxembourgish
+  MO: "zh", // Cantonese Chinese
+  MG: "mg", // Malagasy
+  MW: "ny", // Chichewa
+  MY: "ms", // Malay
+  MV: "dv", // Dhivehi
+  ML: "bm", // Bambara
+  MT: "mt", // Maltese
+  MH: "mh", // Marshallese
+  MQ: "fr", // French
+  MR: "ar", // Arabic
+  MU: "mfe", // Mauritian Creole
+  YT: "fr", // French
+  MX: "es", // Spanish
+  FM: "chk", // Chuukese
+  MD: "ro", // Romanian
+  MC: "fr", // French
+  MN: "mn", // Mongolian
+  ME: "sr", // Serbian
+  MS: "en", // English (sole native)
+  MA: "ar", // Arabic
+  MZ: "pt", // Portuguese
+  MM: "my", // Burmese
+  NA: "af", // Afrikaans
+  NR: "na", // Nauruan
+  NP: "ne", // Nepali
+  NL: "nl", // Dutch
+  NC: "fr", // French
+  NZ: "mi", // Maori (fallback: English)
+  NI: "es", // Spanish
+  NE: "fr", // French
+  NG: "yo", // Yoruba
+  NU: "niu", // Niuean
+  NF: "en", // English (sole native)
+  MK: "mk", // Macedonian
+  MP: "ch", // Chamorro
+  NO: "no", // Norwegian
+  OM: "ar", // Arabic
+  PK: "ur", // Urdu
+  PW: "pau", // Palauan
+  PS: "ar", // Arabic
+  PA: "es", // Spanish
+  PG: "tpi", // Tok Pisin
+  PY: "gn", // Guarani
+  PE: "es", // Spanish
+  PH: "tl", // Filipino/Tagalog
+  PN: "en", // English (sole native)
+  PL: "pl", // Polish
+  PT: "pt", // Portuguese
+  PR: "es", // Spanish
+  QA: "ar", // Arabic
+  RE: "fr", // French
+  RO: "ro", // Romanian
+  RU: "ru", // Russian
+  RW: "rw", // Kinyarwanda
+  BL: "fr", // French
+  SH: "en", // English (sole native)
+  KN: "kea", // Kabuverdianu
+  LC: "kwy", // Kwéyòl
+  MF: "fr", // French
+  PM: "fr", // French
+  VC: "srl", // Vincentian Creole
+  WS: "sm", // Samoan
+  SM: "it", // Italian
+  ST: "pt", // Portuguese
+  SA: "ar", // Arabic
+  SN: "wo", // Wolof
+  RS: "sr", // Serbian
+  SC: "crs", // Seychellois Creole
+  SL: "men", // Mende
+  SG: "zh", // Mandarin Chinese (largest native, but English is official)
+  SX: "nl", // Dutch
+  SK: "sk", // Slovak
+  SI: "sl", // Slovenian
+  SB: "pis", // Pijin
+  SO: "so", // Somali
+  ZA: "zu", // Zulu
+  ES: "es", // Spanish
+  LK: "si", // Sinhala
+  SD: "ar", // Arabic
+  SR: "nl", // Dutch
+  SJ: "no", // Norwegian
+  SZ: "ss", // Swati
+  SE: "sv", // Swedish
+  CH: "de", // German (most native speakers)
+  SY: "ar", // Arabic
+  TW: "zh", // Mandarin Chinese
+  TJ: "tg", // Tajik
+  TZ: "sw", // Swahili
+  TH: "th", // Thai
+  TL: "tet", // Tetum
+  TG: "fr", // French
+  TK: "tkl", // Tokelauan
+  TO: "to", // Tongan
+  TT: "en", // English (sole native)
+  TN: "ar", // Arabic
+  TR: "tr", // Turkish
+  TM: "tk", // Turkmen
+  TC: "en", // English (sole native)
+  TV: "tvl", // Tuvaluan
+  UG: "lg", // Luganda
+  UA: "uk", // Ukrainian
+  AE: "ar", // Arabic
+  GB: "cy", // Welsh (fallback: English)
+  US: "es", // Spanish (largest native after English)
+  UM: "en", // English (sole native)
+  UY: "es", // Spanish
+  UZ: "uz", // Uzbek
+  VU: "bi", // Bislama
+  VE: "es", // Spanish
+  VN: "vi", // Vietnamese
+  VG: "en", // English (sole native)
+  VI: "en", // English (sole native)
+  WF: "fr", // French
+  EH: "ar", // Arabic
+  YE: "ar", // Arabic
+  ZM: "bem", // Bemba
+  ZW: "sn", // Shona"
+};
