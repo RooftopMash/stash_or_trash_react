96fdc54a373a8ca4611a6fe4eb4cbe18c7d36816
RooftopMash <borulelo@gmail.com>
2025-05-29 16:38:38 +0200

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "3rem" }}>
+      <img src="/icons/app-logo.png" alt="App Logo" style={{ width: 120, marginBottom: 20 }} />
+      <h1>Welcome to Stash or Trash???</h1>
+      <p>
+        Please <Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link> to continue.
+      </p>
+      <p style={{ color: "#aaa", fontSize: 14, marginTop: 40 }}>
+        Powered by RooftopMash, React & Firebase.
+      </p>
+    </div>
+  );
+}
