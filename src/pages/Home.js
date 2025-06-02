2b047551db42e13f4a27bfc4527151dad219ca61
RooftopMash <borulelo@gmail.com>
2025-06-02 22:25:37 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import appLogo from "../assets/app-logo.png";
 
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "3rem" }}>
-      <img src="/icons/app-logo.png" alt="App Logo" style={{ width: 120, marginBottom: 20 }} />
+      <img src={appLogo} alt="App Logo" style={{ width: 120, marginBottom: 20 }} />
       <h1>Welcome to Stash or Trash???</h1>
       <p>
         Please <Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link> to continue.
